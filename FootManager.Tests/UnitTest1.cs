c3ef804d425e9aa4d82659631a5c56c7d071b4c8
Tiphaine Accary-Barbier <34951152+taccary@users.noreply.github.com>
2026-07-15 06:14:28 +0000

    ajout tests unitaire et classe Joueur détaillée

--- FootManager.Tests/UnitTest1.cs
+++ FootManager.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿namespace FootManager.Tests;
+﻿using FootManager.Models;
+
+namespace FootManager.Tests;
 
 public class UnitTest1
 {
@@ -8,5 +10,26 @@ public class UnitTest1
         var joueur = new Joueur("Lacazette", 34);
 
         Assert.Equal("Lacazette", joueur.Nom);
+        Assert.Equal(34, joueur.Age);
+    }
+
+    [Fact]
+    public void Creation_Equipe()
+    {
+        var equipe = new Equipe("Arsenal");
+
+        Assert.Equal("Arsenal", equipe.Nom);
+        Assert.Empty(equipe.Joueurs);
+    }
+
+    [Fact]
+    public void Affectation_Joueur_Equipe()
+    {
+        var joueur = new Joueur("Lacazette", 34);
+        var equipe = new Equipe("Arsenal");
+
+        equipe.AjouterJoueur(joueur);
+
+        Assert.Contains(joueur, equipe.Joueurs);
     }
 }
--- FootManager/Models/Joueur.cs
+++ FootManager/Models/Joueur.cs
@@ -2,17 +2,50 @@ namespace FootManager.Models;
 
 public class Joueur
 {
-    public string Nom { get; set; }
-    public int Age { get; set; }
+    private string _nom; // Cattribut privé pour stocker le nom 
+    private int _age; // attribut privé pour stocker l'âge
 
+
+    // Constructeur qui valorise les attributs privés _nom et _age avec les valeurs passées en paramètres
     public Joueur(string nom, int age)
     {
-        Nom = nom;
-        Age = age;
+        this._nom = nom;
+        this._age = age;
     }
 
+
+    // Propriété Nom avec accesseurs get et set et validation du nom dans le set
+    public string Nom
+    {
+        get { return this._nom; }
+        set { 
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Nom invalide");
+            }
+            this._nom = value; 
+            }
+    }
+
+    // Propriété Age avec accesseurs get et set et validation de l'âge dans le set
+    public int Age
+    {
+        get { return this._age; }
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age invalide");
+            }
+
+            this._age = value;
+        }
+    }
+
+    // Redéfinition de la méthode ToString pour retourner une représentation textuelle de l'objet Joueur
     public override string ToString()
     {
-        return $"{Nom} ({Age} ans)";
+        return $"{this.Nom} ({this.Age} ans)";
     }
 }
